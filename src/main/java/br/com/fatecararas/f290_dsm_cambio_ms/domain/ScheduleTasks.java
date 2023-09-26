d779041b2ca7425b19cff46c56c5238e62c7e695
BZR4 <BZR4@users.noreply.github.com>
2023-09-25 21:07:56 -0300

    Alteração de chave

--- src/main/java/br/com/fatecararas/f290_dsm_cambio_ms/domain/ScheduleTasks.java
+++ src/main/java/br/com/fatecararas/f290_dsm_cambio_ms/domain/ScheduleTasks.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
